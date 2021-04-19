f4cf1b5e993baf9042feab12b8dbaca2317e07e5
smh31 <seyed_mahdi_hosseini_miangoleh@sfu.ca>
2021-04-19 12:20:42 -0700

    adding the rotation into the grassDataset Class

--- SCORES/grassdata.py
+++ SCORES/grassdata.py
@@ -49,6 +49,42 @@ class Tree(object):
         assert len(queue) == 1
         self.root = queue[0]
 
+def rotate_boxes(boxes, syms):
+    new_boxes = torch.zeros(boxes.shape)
+    for k in range(boxes.shape[0]):
+        new_boxes[k, 0] = boxes[k, 2]
+        new_boxes[k, 1] = boxes[k, 1]
+        new_boxes[k, 2] = -boxes[k, 0]
+
+        new_boxes[k, 3] = boxes[k, 3]
+        new_boxes[k, 4] = boxes[k, 5]
+        new_boxes[k, 5] = boxes[k, 4]
+
+        new_boxes[k, 6] = boxes[k, 6]
+        new_boxes[k, 7] = boxes[k, 7]
+        new_boxes[k, 8] = boxes[k, 8]
+
+        new_boxes[k, 9] = boxes[k, 9]
+        new_boxes[k, 10] = boxes[k, 10]
+        new_boxes[k, 11] = boxes[k, 11]
+
+    # rotate syms
+    new_syms = torch.zeros(syms.shape)
+    for k in range(syms.shape[0]):
+        new_syms[k, 0] = syms[k, 0]
+
+        new_syms[k, 1] = syms[k, 3]
+        new_syms[k, 2] = syms[k, 2]
+        new_syms[k, 3] = -syms[k, 1]
+
+        new_syms[k, 4] = syms[k, 6]
+        new_syms[k, 5] = syms[k, 5]
+        new_syms[k, 6] = -syms[k, 4]
+
+        new_syms[k, 7] = syms[k, 7]
+    return new_boxes, new_syms
+
+
 
 class GRASSDataset(data.Dataset,):
     def __init__(self, dir, models_num=0, transform=None):
@@ -56,13 +92,19 @@ class GRASSDataset(data.Dataset,):
         num_examples = len(os.listdir(os.path.join(dir, 'ops')))
         self.transform = transform
         self.trees = []
+        self.Ids = []
         for i in range(models_num):
             boxes = torch.from_numpy(loadmat(os.path.join(dir, 'boxes', '%d.mat' % (i+1)))['box']).t().float()
             ops = torch.from_numpy(loadmat(os.path.join(dir, 'ops', '%d.mat' % (i+1)))['op']).int()
             syms = torch.from_numpy(loadmat(os.path.join(dir, 'syms', '%d.mat' % (i+1)))['sym']).t().float()
             labels = torch.from_numpy(loadmat(os.path.join(dir, 'labels', '%d.mat' % (i+1)))['label']).int()
-            tree = Tree(boxes, ops, syms, labels)
+            shapeId = loadmat(os.path.join(dir, 'part mesh indices', '%d.mat' % (i+1)))['shapename']
+
+            new_boxes1, new_syms1 = rotate_boxes(boxes, syms)
+            tree = Tree(new_boxes1, ops, new_syms1, labels)
+
             self.trees.append(tree)
+            self.Ids.append(shapeId)
 
     def __getitem__(self, index):
         tree = self.trees[index]
